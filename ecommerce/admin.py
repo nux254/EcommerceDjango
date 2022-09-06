f76d5ecacfff6defb06b5771fd2a62ea9cb1f333
nux254 <apps.nuxtechnology@gmail.com>
2022-09-06 15:16:07 +0300

    ecommerce-admin

---
+++ ecommerce/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from ecommerce.models import Categories,SubCategories
+
+# Register your models here.
+admin.site.register(Categories)
+admin.site.register(SubCategories)
